e4447881dc5154202eae1f4f539351ae694a638a
bunn1 <nicjohbun@gmail.com>
2024-02-22 10:34:41 +0100

    inserting component in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import Contact from './components/Contact'; // Import the Contact component
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Welcome to My Website</h1>
+      <Contact /> {/* Include the Contact component here */}
+    </div>
+  );
 }
 
 export default App;
