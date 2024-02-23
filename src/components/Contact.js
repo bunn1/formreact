cc572c88969605ff07b51c6d8a8b68f29f49fd04
bunn1 <nicjohbun@gmail.com>
2024-02-23 12:28:02 +0100

    package.json

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,92 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Contact() {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
   const [error, setError] = useState('');
+  const [selectData, setSelectData] = useState([]);
+  const [selectValue, setSelectValue] = useState('');
+
+  useEffect(() => {
+    let processing = true;
+
+    axiosFetchData(processing);
+
+    return () => {
+      processing = false;
+    };
+  }, []);
+
+  // const fetchData = async (processing) => {
+  // Sätt in variabeln option efter https adressen beroende på vem du skall ha post el get
+
+  // await fetch('https://jsonplaceholder.typicode.com/users', option)
+
+  // const option = {
+  //   method: 'POST',
+  //   headers: { 'Content-type': 'application/json' },
+  //   body: JSON.stringify({
+  //     email: email,
+  //     message: message,
+  //   }),
+  // };
+
+  //   await fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       if (processing) {
+  //         setSelectData(data);
+  //       }
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
+
+  const axiosFetchData = async (processing) => {
+    // Vid metoden post är det bara skriva in den här variabeln
+    // .post('https://jsonplaceholder.typicode.com/users', options)
+    // const options = {
+    //   email: email,
+    //   message: message,
+    // };
+
+    await axios
+      // Går att välja post, get etc direkt
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        if (processing) {
+          setSelectData(res.data);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const SelectDropdown = () => {
+    return (
+      <select
+        value={selectValue}
+        onChange={(e) => setSelectValue(e.target.value)}
+      >
+        {selectData?.map((item) => (
+          <option value={item.website} key={item.website}>
+            {item.website}
+          </option>
+        ))}
+      </select>
+    );
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log(email + ' | ' + message);
+    console.log(email + ' | ' + selectValue + ' | ' + message);
 
     if (!message) {
       setError(
         <p className="required">Message is empty. Please type a message</p>
       );
+    } else {
+      setError('');
     }
   };
 
@@ -30,6 +103,9 @@ function Contact() {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+
+        <label>How did you hear about us?</label>
+        <SelectDropdown />
 
         <label>Message</label>
         <textarea
